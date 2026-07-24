a443ba16df55dbd294c84d0a0446275c30ce1d21
vandna-kumari <vandnakpr@gmail.com>
2026-07-24 14:18:32 +0530

    feat: whats open

--- utils/whatsapp.ts
+++ utils/whatsapp.ts
@@ -1,4 +1,4 @@
-export const sendWhatsAppMessage = (message: string, phoneNumber: string = "919530802147") => {
+export const sendWhatsAppMessage = (message: string, phoneNumber: string = "919368662818") => {
   const encodedMessage = encodeURIComponent(message);
   window.open(`https://wa.me/${phoneNumber}?text=${encodedMessage}`, "_blank");
 };
